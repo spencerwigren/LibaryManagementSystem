61895ab410d862bafa7d5ad715e4eb4085e69c63
Spencer Wigren <77368960+spencerwigren@users.noreply.github.com>
2025-02-06 11:59:14 -0700

    Got sqlite basic database working

--- main.go
+++ main.go
@@ -41,7 +41,10 @@ func main() {
 	checkError(err)
 	defer db.Close()
 
-	addBookInfo("testBook", 125, db, err)
+	addUserInfo("Admin", db, err)
+	addBookInfo("testBook", 125, "Admin", db, err)
+	addMovieInfo("testMoive", db, err)
+	addVideoGameInfo("testVideoGame", db, err)
 	queryDB(db, err)
 
 }
@@ -52,62 +55,192 @@ func initdatabase() {
 	checkError(err)
 	defer db.Close()
 
+	// drop table
+	dropTables(db, err)
+
 	// Create a table
-	createTableSQL := `CREATE TABLE IF NOT EXISTS users (
+	createTableSQL := `
+	CREATE TABLE IF NOT EXISTS users (
 		id INTEGER PRIMARY KEY AUTOINCREMENT,
-		name TEXT NOT NULL,
-		age INTEGER
-	);`
+		name TEXT NOT NULL
+	);
+	
+	CREATE TABLE IF NOT EXISTS books (
+	    bookId INTEGER PRIMARY KEY AUTOINCREMENT,
+	    title TEXT NOT NULL,
+		pageNumber INTEGER NOT NULL,
+	    author TEXT NOT NULL
+	);
+	
+	CREATE TABLE IF NOT EXISTS movies(
+		movieId INTEGER PRIMARY KEY AUTOINCREMENT, 
+		title TEXT NOT NULL
+	);
+	
+	CREATE TABLE IF NOT EXISTS videoGames (
+		videoGameId INTEGER PRIMARY KEY AUTOINCREMENT,
+		title TEXT NOT NULL
+	)`
 
 	_, err = db.Exec(createTableSQL)
 	checkError(err)
 
-	fmt.Println("Database and table created successfully!")
+	log.Println("Database and tables created successfully!")
+
+}
+
+func dropTables(db *sql.DB, err error) {
+
+	checkError(err)
+	// // Drop users table
+	// tableName := "users"
+	// query := fmt.Sprintf("DROP TABLE IF EXISTS %s;", tableName)
+
+	// _, err = db.Exec(query)
+	// if err != nil {
+	// 	log.Fatal("Failed to delete table:", err)
+	// }
+
+	// //Drop books table
+	// tableName = "books"
+	// query = fmt.Sprintf("DROP TABLE IF EXISTS %s;", tableName)
+
+	// _, err = db.Exec(query)
+	// if err != nil {
+	// 	log.Fatal("Faild to delete table:", err)
+	// }
+
+	// //Drop moive table
+	// tableName = "movies"
+	// query = fmt.Sprintf("DROP TABLE IF EXISTS %s;", tableNam)
+
+	tables := []string{"users", "books", "movies", "videoGames"}
+
+	for _, table := range tables {
+		query := fmt.Sprintf("DROP TABLE IF EXISTS %s;", table)
+		_, err := db.Exec(query)
+		if err != nil {
+			log.Fatalf("Failed to delete table %s: %v", table, err)
+		}
+		fmt.Printf("Table %s deleted successfully!\n", table)
+	}
+
+	log.Println("Tables deleted successfully!")
 
 }
 
 func queryDB(db *sql.DB, err error) {
-	rows, err := db.Query("SELECT id, name, age FROM users;")
 	checkError(err)
-	defer rows.Close()
+	userRows, err := db.Query("SELECT id, name FROM users;")
+	checkError(err)
+	defer userRows.Close()
 
-	fmt.Println("Users:")
-	for rows.Next() {
+	fmt.Println("\nUsers:")
+	for userRows.Next() {
 		var id int
 		var name string
-		var age int
 
-		err = rows.Scan(&id, &name, &age)
+		err = userRows.Scan(&id, &name)
 		checkError(err)
 
-		fmt.Printf("ID: %d, Name: %s, Age: %d\n", id, name, age)
+		fmt.Printf("ID: %d, Name: %s\n", id, name)
 	}
 
-	if err = rows.Err(); err != nil {
+	if err = userRows.Err(); err != nil {
 		log.Fatal(err)
 	}
+
+	bookRows, err := db.Query("SELECT bookId, title, pageNumber, author FROM books;")
+	checkError(err)
+	defer bookRows.Close()
+
+	fmt.Println("\nBooks:")
+	for bookRows.Next() {
+		var bookId int
+		var title string
+		var pageNumber int
+		var author string
+
+		err = bookRows.Scan(&bookId, &title, &pageNumber, &author)
+		checkError(err)
+
+		fmt.Printf("Book ID: %d, Title: %s, Page Number %d, Author %s\n", bookId, title, pageNumber, author)
+	}
+
+	if err = bookRows.Err(); err != nil {
+		log.Fatal(err)
+	}
+
+	movieRows, err := db.Query("SELECT movieId, title FROM movies")
+	checkError(err)
+	defer movieRows.Close()
+
+	fmt.Println("\nMovies:")
+	for movieRows.Next() {
+		var movieId int
+		var title string
+
+		err = movieRows.Scan(&movieId, &title)
+		checkError(err)
+
+		fmt.Printf("Movie ID: %d, Title: %s\n", movieId, title)
+
+	}
+
+	if err = movieRows.Err(); err != nil {
+		log.Fatal(err)
+	}
+
+	videoGameRows, err := db.Query("SELECT videoGameId, title FROM videoGames")
+	checkError(err)
+	defer videoGameRows.Close()
+
+	fmt.Println("\nVideo Games:")
+	for videoGameRows.Next() {
+		var videoGameId int
+		var title string
+
+		err = videoGameRows.Scan(&videoGameId, &title)
+		checkError(err)
+
+		fmt.Printf("Video Games ID: %d, Title: %s\n", videoGameId, title)
+	}
+
 }
 
-func addBookInfo(title string, pageNumber int, db *sql.DB, err error) {
-	// b := book{title: title}
-	// b.pageNumber = pageNumber
-	// b.author = author
+func addBookInfo(title string, pageNumber int, author string, db *sql.DB, err error) {
+	checkError(err)
 
-	insertSQL := `INSERT INTO users (name, age) VALUES (?, ?)`
-	_, err = db.Exec(insertSQL, title, pageNumber)
+	insertSQL := `INSERT INTO books (title, pageNumber, author) VALUES (?, ?, ?)`
+	_, err = db.Exec(insertSQL, title, pageNumber, author)
 	checkError(err)
 
 }
 
-// func addMovieInfo(title string) *movie {
-// 	m := movie{title: title}
-// 	return &m
-// }
+func addMovieInfo(title string, db *sql.DB, err error) {
+	checkError(err)
 
-// func addVideoGameInfo(title string) *videoGame {
-// 	g := videoGame{title: title}
-// 	return &g
-// }
+	insertSQL := `INSERT INTO movies (title) VALUES (?)`
+	_, err = db.Exec(insertSQL, title)
+	checkError(err)
+
+}
+
+func addVideoGameInfo(title string, db *sql.DB, err error) {
+	checkError(err)
+
+	insertSQL := `INSERT INTO videoGames (title) values (?)`
+	_, err = db.Exec(insertSQL, title)
+	checkError(err)
+}
+
+func addUserInfo(name string, db *sql.DB, err error) {
+	checkError(err)
+
+	insertSQL := `INSERT INTO users (name) 	VALUES (?)`
+	_, err = db.Exec(insertSQL, name)
+	checkError(err)
+}
 
 func checkError(err error) {
 	if err != nil {
