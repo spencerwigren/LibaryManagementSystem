f0fe97750a471f3433c372d935d1cac51511a2f7
Spencer Wigren <77368960+spencerwigren@users.noreply.github.com>
2025-04-03 16:28:31 -0600

    Working Update Media Pages

--- utils/updatedb.go
+++ utils/updatedb.go
@@ -1,7 +1,59 @@
 package utils
 
-import "database/sql"
+import (
+	"database/sql"
+	"log"
+	"time"
+)
 
-func updateEntry(db *sql.DB) {
+func UpdateEntryBook(db *sql.DB, titleUpdate string, pageNum int, authUpdate string, orgTitle string) {
+
+	updateQuery := "UPDATE books SET title = ?, pageNumber = ?, author = ?, time = ? WHERE title = ?"
+	row, err := db.Exec(updateQuery, titleUpdate, pageNum, authUpdate, time.Now(), orgTitle)
+	if err != nil {
+		log.Println("ERROR", err)
+		return
+	}
+
+	rowsAffected, err := row.RowsAffected()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	log.Printf("UPDATE %d rows(s)\n", rowsAffected)
+
+}
+
+func UpdateEntryMovie(db *sql.DB, titleUpdate string, ratingUpdate string, yearConverted int, orgTitle string) {
+	updateQuery := "UPDATE movie SET title = ?, rating = ?, releaseYear = ?, time = ? WHERE title = ?"
+	row, err := db.Exec(updateQuery, titleUpdate, ratingUpdate, yearConverted, time.Now(), orgTitle)
+	if err != nil {
+		log.Println("ERROR", err)
+		return
+	}
+
+	rowsAffected, err := row.RowsAffected()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	log.Printf("UPDATE %d rows(s)\n", rowsAffected)
+
+}
+
+func UpdateEntryGames(db *sql.DB, titleUpdate string, ratingUpdate string, yearConverted int, orgTitle string) {
+	updateQuery := "UPDATE videoGames SET title = ?, rating = ?, releaseYear = ?, time = ? WHERE title = ?"
+	row, err := db.Exec(updateQuery, titleUpdate, ratingUpdate, yearConverted, time.Now(), orgTitle)
+	if err != nil {
+		log.Println("ERROR", err)
+		return
+	}
+
+	rowsAffected, err := row.RowsAffected()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	log.Printf("UPDATE %d rows(s)\n", rowsAffected)
 
 }
